76f5f5f9103e75ff3794c372db7d3986db8edfe9
Pedro Henrique Reis Lima <160922340+Pedro-Pratina@users.noreply.github.com>
2026-01-14 15:36:49 -0300

    feat: estrutura inicial do projeto Cria a estrutura de pastas e arquivos base para iniciar o desenvolvimento.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { Beef } from "lucide-react";
 
 export function App() {
   return (
-    <Routes>
-      <Route path="/" element={<h1>Test <span className="text-cyan-300">Taiwind</span> e <span className="text-fuchsia-600">Lucide <Beef /></span></h1>} />
-    </Routes>
+    <BrowserRouter>
+      <null /> {/* Placeholder for Navbar*/} 
+
+      <Routes>
+        <Route path="/" element={null} />
+        <Route path="/projetos/:id" element={null} />
+      </Routes>
+
+      <null /> {/* Placeholder for Footer*/}
+    </BrowserRouter>
   );
 }
